5f4c44fa3ab6c115532ba08be3d4f5bcb3554862
K4NTOS <153729977+GianRusellFlores@users.noreply.github.com>
2026-06-18 15:22:12 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import mysql.connector
 
 def get_db_connection():
@@ -7,14 +6,4 @@ def get_db_connection():
         user="root",
         password="",
         database="agriculture_dms"
-=======
-import mysql.connector
-
-def get_db_connection():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="agriculture_dms"
->>>>>>> a72cffaba9099136b3b23b3b7f422b5cd19dd79b
     )
